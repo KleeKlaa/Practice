1c1b4f656d339496bd9b80497b991e5b1f973692
Elke Kalka <elke.kalka@wabion.com>
2020-12-14 20:01:17 +0100

    #Day 1

---
+++ DayOne.java
@@ -0,0 +1,7 @@
+public class _C {
+    2: private static int $;
+    3: public static void main(String[] main) {
+    4: String a_b;
+    5: System.out.print($);
+    6: System.out.print(a_b);
+    7: } }
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Practice
 to practice
+
+practice - the beginning
+
+1. create in github and clone to visual studio code
